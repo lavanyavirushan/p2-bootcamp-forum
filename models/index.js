3c4c200f1fdb9ec06eea6f24824f514e7e6eb74c
MadelineLowes <madeline.lowes@gmail.com>
2023-01-23 21:17:25 -0500

    added the base folders and files to the repo

---
+++ models/index.js
@@ -0,0 +1,58 @@
+const User = require('./User');
+const Hashtag = require('./Tag');
+const UserPost = require('./UserPost');
+const Comment = require('./Comment');
+
+// user & userpost
+User.hasMany(UserPost, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+UserPost.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// user & comment
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// userpost & comment
+UserPost.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(UserPost, {
+    foreignKey: 'user_id'
+});
+
+// user and hashtag
+User.hasMany(Hashtag, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Hashtag.hasMany(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// hashtag & userpost
+Hashtag.hasMany(UserPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+UserPost.hasMany(Hashtag, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, UserPost, Hashtag, Comment };
---
+++ seeds/seed.js
@@ -0,0 +1,28 @@
+const sequelize = require('../config/connection');
+const { User, UserPost, Tag, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const userPostData = require('./userPostData.json');
+const tagData = require('./tagData.json');
+const commentData = require('./commentData.json');
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+//   for (const project of projectData) {
+//     await Project.create({
+//       ...project,
+//       user_id: users[Math.floor(Math.random() * users.length)].id,
+//     });
+//   }
+
+  process.exit(0);
+};
+
+seedDatabase();
