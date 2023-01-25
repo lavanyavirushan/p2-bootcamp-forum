88686e7d8c192555b0ab09f1b00f0a2d06c59a93
unknown <philipkurianv@gmail.com>
2023-01-24 20:58:18 -0500

    added code for the index model

--- models/index.js
+++ models/index.js
@@ -1,15 +1,15 @@
 const User = require('./User');
-const Hashtag = require('./Tag');
-const UserPost = require('./UserPost');
+const Category = require('./Category');
+const Post = require('./UserPost');
 const Comment = require('./Comment');
 
 // user & userpost
-User.hasMany(UserPost, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-UserPost.belongsTo(User, {
+Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
@@ -24,35 +24,24 @@ Comment.belongsTo(User, {
 });
 
 // userpost & comment
-UserPost.hasMany(Comment, {
-    foreignKey: 'user_id',
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
     onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(UserPost, {
-    foreignKey: 'user_id'
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
 });
 
-// user and hashtag
-User.hasMany(Hashtag, {
-    foreignKey: 'user_id',
+
+// Category & userpost
+Post.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
+
+Category.hasMany(Post, {
+    foreignKey: 'category_id',
     onDelete: 'CASCADE'
 });
 
-Hashtag.hasMany(User, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
-// hashtag & userpost
-Hashtag.hasMany(UserPost, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
-UserPost.hasMany(Hashtag, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
-module.exports = { User, UserPost, Hashtag, Comment };
+module.exports = { User, Post, Category, Comment };
