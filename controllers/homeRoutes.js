d7f5f18ba6bd875a70939e77ed79e792ed034254
unknown <philipkurianv@gmail.com>
2023-01-26 18:03:41 -0500

    added authentication cookies and changed home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,39 +4,39 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
 
-  });
+});
   
-  router.get('/project/:id', async (req, res) => {
+router.get('/project/:id', async (req, res) => {
 
-  });
+});
   
   // Use withAuth middleware to prevent access to route
-  router.get('/profile', withAuth, async (req, res) => {
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Project }],
-      });
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
   
-      const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
   
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
     } catch (err) {
       res.status(500).json(err);
     }
   });
   
-  router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
-    }  
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }  
     res.render('login');
   });
   
-  module.exports = router;
+module.exports = router;
