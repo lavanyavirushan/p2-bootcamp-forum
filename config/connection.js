3b684257c55dad5797a0db831367c72980bb9ce9
Lavanya Virushan <lavanya.virushan@gmail.com>
2023-01-30 18:45:08 -0500

    fixed merge conflict

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 let sequelize;
 
-<<<<<<< lavi-update-connection
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
@@ -18,17 +17,5 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
-=======
-sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT
-  }
-);
->>>>>>> development
 
 module.exports = sequelize;
