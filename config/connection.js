8c46bf5d981bbe7d1ae3d17f759d4c2856de15cb
Lavanya Virushan <lavanya.virushan@gmail.com>
2023-01-30 18:10:04 -0500

    updated connection string

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,19 @@ require('dotenv').config();
 
 let sequelize;
 
-sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: process.env.DB_PORT
+    }
+  );
+}
 
 module.exports = sequelize;
